19da78f7a37f5a9bfd9c16983f5199764ccf77ff
Yahia mahmoud <yahiamahmoood333@gmail.com>
2025-09-16 23:20:45 +0300

    added rowan, salsabel, mohamed plots

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from db import Cassandra
+import matplotlib.pyplot as plt
 
 @st.cache_resource
 def get_cassandra():
@@ -11,6 +12,13 @@ data = cassandra.join_roads()
 
 st.title("Pavement eye")
 
+
 st.markdown("###### Data")
 st.dataframe(data)
 
+# ----------------------------------------------------------------------------------
+fig, ax = plt.subplots(1, 1)
+
+plot = data.groupby('label')['id'].count()
+ax.pie(plot, labels=plot.index, autopct='%.2f%%')
+st.pyplot(fig)
